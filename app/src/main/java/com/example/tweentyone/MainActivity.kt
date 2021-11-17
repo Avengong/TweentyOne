ade609705044ebb7b5adf9fc373d297674772010
gongwen <635583021@qq.com>
2021-11-17 10:09:08 +0800

    [Initial] 第一次提交

---
+++ app/src/main/java/com/example/tweentyone/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tweentyone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
